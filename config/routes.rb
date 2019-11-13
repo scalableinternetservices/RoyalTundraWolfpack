851ddd07934cdde4847b8f0aea79273d3619a45f
matthew ho <matthewho@umail.ucsb.edu>
2019-11-13 13:53:41 -0800

    Posts UI update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root 'application#index'
-=======
   root "posts#index"
->>>>>>> 68bf5ccd1bfd508946d5ca36df9deb90f9b5f1b7
 end
