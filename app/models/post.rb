39f8a49462f4216249412fd650a1102425238425
matthew ho <matthewho@umail.ucsb.edu>
2019-11-14 16:17:02 -0800

    New Posts Attribute: content

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
 	validates :title, presence: { message: "cannot be empty" }
 	validates :author, presence: { message: "cannot be empty" }
 	validates :upvotes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validates :content, presence: { message: "cannot be empty" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_032000) do
+ActiveRecord::Schema.define(version: 2019_11_14_233530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_032000) do
     t.integer "upvotes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content"
   end
 
 end
