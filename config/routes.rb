b693dc6207b902218d62e24c21ab7410c0143c18
jenniferlai43 <jenniferlai43@gmail.com>
2019-11-18 15:45:58 -0800

    Sign out route signs out instead of setting id to 'sign_out'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # resources :messages
   resources :conversations do
   	resources :messages
