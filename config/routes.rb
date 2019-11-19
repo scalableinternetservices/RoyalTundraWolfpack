5a06ff881aaad9793078af770e0b02a87fff0427
jenniferlai43 <jenniferlai43@gmail.com>
2019-11-18 16:06:57 -0800

    Renamed devise user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
