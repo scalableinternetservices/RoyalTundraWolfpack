62e8df95852c1020e9e11dd23e0773b5695f972d
matthew ho <matthewho@umail.ucsb.edu>
2019-11-19 16:45:03 -0800

    Authentication for posts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -104,7 +104,6 @@ class MessagesController < ApplicationController
   before_action :set_conversation
 
   def create
-
     receipt = current_user.reply_to_conversation(@conversation, params[:body])
     redirect_to conversation_path(receipt.conversation)
   end
