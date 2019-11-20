8d1be664cec261b110ab01a8830a3e47c4473872
jenniferlai43 <jenniferlai43@gmail.com>
2019-11-19 16:08:00 -0800

    New convo with user of existing convo will redirect to that existing convo

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -11,14 +11,30 @@ class ConversationsController < ApplicationController
   end
 
   def new
+    # @recipients = []
+    # User.all.each do |u| 
+    #   if u != current_user
+    #     ongoing_conversation = Mailboxer::Conversation.between(u, current_user).find{|c| c.participants.count == 2 }
+    #     if not ongoing_conversation.present?
+    #       @recipients.append(u)
+    #     end
+    #   end
+    # end
     @recipients = User.all - [current_user]
+    # Conversation.participant(@a).participant(@b)
+    # ADD FILTER FOR NOT ALLOWING NEW CONVOS W PPL WHO ALREADY HAVE CONVO WITH YOU
   end
 
   def create
     # TODO: if user sending to person that already has conversation, use that conversation instead of starting new one
     recipient = User.find(params[:user_id])
-    receipt = current_user.send_message(recipient, params[:body], params[:subject])
-    redirect_to conversations_path(receipt.conversation)
+    ongoing_conversation = Mailboxer::Conversation.between(recipient, current_user).find{|c| c.participants.count == 2 }
+    if ongoing_conversation.present?
+      receipt = current_user.reply_to_conversation(ongoing_conversation, params[:body])
+      redirect_to conversation_path(receipt.conversation)
+    else
+      receipt = current_user.send_message(recipient, params[:body], params[:subject])
+      redirect_to conversation_path(receipt.conversation)
+    end
   end
-
 end
