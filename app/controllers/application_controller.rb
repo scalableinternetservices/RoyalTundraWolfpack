3bf006a8c3092c201a01ca47e948c19bc498052c
jenniferlai43 <jenniferlai43@gmail.com>
2019-11-21 15:10:17 -0800

    Fixed login - don't need username to authenticate login, only register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
   protected 
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
     end
 end
