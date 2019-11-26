b0877f367f158bc3985203bd99337d29b843587d
matthew ho <matthewho@umail.ucsb.edu>
2019-11-26 00:48:08 -0800

    Comment count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :commentable, polymorphic: true
 	has_many :comments, as: :commentable
+
+	def commentCount
+		total = comments.count
+		comments.each do |child_comment|
+			total += child_comment.commentCount
+		end
+		return total
+  end
 end
