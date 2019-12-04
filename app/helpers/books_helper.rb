1de73145c052aec2aec1016ea9d11fe2c1b3488a
matthew ho <matthewho@umail.ucsb.edu>
2019-12-04 04:42:03 -0800

    fragment caching

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,17 @@ module BooksHelper
     end
     return auth.username
   end
+
+  def cache_key_for_post(post)
+    return "post/#{post.id}"
+  end
+
+  def truncate_content(content)
+    if content.length <= 200
+      return content
+    else
+      s = content.split(" ").each_with_object("") {|x,ob| break ob unless (ob.length + " ".length + x.length <= 200);ob << (" " + x)}.strip
+      return s + "..."
+    end
+  end
 end
