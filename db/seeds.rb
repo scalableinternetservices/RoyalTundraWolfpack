130671cfc32c86a2ad8950bde6de8e6d573944c0
jenniferlai43 <jenniferlai43@gmail.com>
2019-12-05 00:56:11 -0800

    Store less memory on seed and remove cache from books index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,86 +1,95 @@
-posts = []
-childcomments = []
-parentcomments= []
+# posts = []
+# childcomments = []
+# parentcomments= []
 users = []
-books = []
+# books = []
 
+USER_COUNT = 100
+BOOK_COUNT = 50
+POST_COUNT = USER_COUNT*BOOK_COUNT
+
+USER_PARENT_COMMENT_CAP = 10
+PARENT_COMMENT_COUNT = USER_PARENT_COMMENT_CAP*POST_COUNT
+
+USER_CHILD_COMMENT_CAP = 2
+CHILD_COMMENT_COUNT = USER_CHILD_COMMENT_CAP*PARENT_COMMENT_COUNT
+
+
+
+uc = 0
 i = 0
-while i < 100
-  user = User.new(email: i.to_s + "@seed.com", username: "seed" + i.to_s, password: "asdasd", password_confirmation: "asdasd")
+while i < USER_COUNT
+  user = User.create(email: i.to_s + "@seed.com", username: "seed" + i.to_s, password: "asdasd", password_confirmation: "asdasd")
   users << user
   i += 1
+  uc += 1
 end
 
-User.import users, validate: false
-
+bc = 0
 i = 0
-while i < 50
-  book = Book.new(title: "seedbook" + i.to_s, author: "author" + i.to_s)
-  books << book
+while i < BOOK_COUNT
+  Book.create(title: "seedbook" + i.to_s, author: "author" + i.to_s)
+  i += 1
+  bc += 1
+end
+
+
+pc = 0
+i = 0
+while i < USER_COUNT
+  j = 0
+  while j < BOOK_COUNT
+    k = 0
+    while k < 1
+      Post.create(title: "seed_title" + i.to_s, author: i+1, content: "content", book_id: j+1)
+      k+=1
+      pc += 1
+    end
+    j+=1
+  end
+  i+=1
+end 
+
+pcc = 0 
+i = 0
+while i < USER_PARENT_COMMENT_CAP
+  j = 0
+  while j < POST_COUNT
+    k = 0
+    while k < 1
+      Comment.create(body: "parent_comment_body", commentable_id: j+1, commentable_type: "Post", user_id: i+1)
+      k += 1
+      pcc += 1
+    end
+    j += 1
+  end
   i += 1
 end
 
-Book.import books, validate: false
-
-
-users.each do |user|
-  books.each do |book|
-    j = 0
-    while j < 1
-      post = Post.new(title: user.username + "_" + j.to_s, author: user.id, content: "content", book_id: book.id)
-      # puts "post:" + post.title
-      posts << post
-
-      j += 1
-    end
-  end
-end
-
-Post.import posts, validate: false
-
-pc_count = 0
-#comments
-(0..9).each do |user_id|
-  posts.each do |post|
-    parent_count = 0
-    while parent_count < 1
-      parent_comment = Comment.new(body: "comment_body", commentable_id: post.id, commentable_type: "Post", user_id: user_id)
-      # puts "pcomment: " + parent_comment.commentable_id.to_s
-      parentcomments << parent_comment
-      parent_count += 1
-    end
-    pc_count += 1
-    # puts "pcomment count: " + pc_count.to_s
-  end
-end
-Comment.import parentcomments, validate: false
-
-
-cc_count = 0
-# users.each do |user|
-(0..1).each do |user_id|
-  parentcomments.each do |parent_comment|
+ccc = 0
+i = 0
+while i < USER_CHILD_COMMENT_CAP
+  j = 0
+  while j < USER_PARENT_COMMENT_CAP
     k = 0
     while k < 2
-      child_comment = Comment.new(body: "comment_body", commentable_id: parent_comment.id, commentable_type: "Comment", user_id: user_id)
-      # puts "ccomment: " + child_comment.commentable_id.to_s
-      childcomments << child_comment
-      # puts "ccomment count: " + k.to_s
+      Comment.create(body: "child_comment_body", commentable_id: j+1, commentable_type: "Comment", user_id: i+1)
       k += 1
-      cc_count += 1
-      # puts "ccomment count: " + cc_count.to_s
+      ccc += 1
     end
+    j += 1
   end
+  i += 1
 end
 
-Comment.import childcomments, validate: false
+# Comment.import childcomments, validate: false
 
 users.each do |sender|
   users.each do |receiver|
     if sender != receiver
       ongoing_conversation = Mailboxer::Conversation.between(receiver, sender).find{|c| c.participants.count == 2 }
       if !ongoing_conversation.present?
-        receipt = sender.send_message(receiver, messageList.sample, "default-subject")
+        receipt = sender.send_message(receiver, "seed message", "default-subject")
       end
     end
   end
@@ -97,7 +106,7 @@ users.each do |sender|
       if ongoing_conversation.present?
         i = 0
         while i < 10
-          receipt = sender.reply_to_conversation(ongoing_conversation, messageList.sample)
+          receipt = sender.reply_to_conversation(ongoing_conversation, "seed reply")
           i += 1
         end
       end
