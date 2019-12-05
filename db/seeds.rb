31822e4cae3265769aaffade8598d4cdc7d129e7
jenniferlai43 <jenniferlai43@gmail.com>
2019-12-05 01:41:14 -0800

    Fixed seed loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,19 +68,34 @@ end
 
 ccc = 0
 i = 0
-while i < USER_CHILD_COMMENT_CAP
+
+while i < PARENT_COMMENT_COUNT
   j = 0
-  while j < USER_PARENT_COMMENT_CAP
+  while j < USER_CHILD_COMMENT_CAP
     k = 0
     while k < 2
-      Comment.create(body: "child_comment_body", commentable_id: j+1, commentable_type: "Comment", user_id: i+1)
+      Comment.create(body: "child_comment_body", commentable_id: i+1, commentable_type: "Comment", user_id: j+1)
       k += 1
-      ccc += 1
     end
     j += 1
   end
   i += 1
 end
+
+
+# while i < USER_CHILD_COMMENT_CAP
+#   j = 0
+#   while j < PARENT_
+#     k = 0
+#     while k < 2
+#       Comment.create(body: "child_comment_body", commentable_id: j+1, commentable_type: "Comment", user_id: i+1)
+#       k += 1
+#       ccc += 1
+#     end
+#     j += 1
+#   end
+#   i += 1
+# end
 
 # Comment.import childcomments, validate: false
 
