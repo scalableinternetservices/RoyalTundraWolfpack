77899ebaf78bcc77de5653dbc1b3601c81d2f980
jenniferlai43 <jenniferlai43@gmail.com>
2019-12-09 17:40:13 -0800

    Add authenticate to create book and remove upvotes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  prepend_before_action :authenticate_user!, except: [:index, :show]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
